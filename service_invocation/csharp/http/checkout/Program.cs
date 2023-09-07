525c42c6a889d8b00b02820cd8e024be8e2afc96
MregXN <mregxn@gmail.com>
2023-09-07 17:11:57 +0800

    csharp
    Signed-off-by: MregXN <mregxn@gmail.com>

--- service_invocation/csharp/http/checkout/Program.cs
+++ service_invocation/csharp/http/checkout/Program.cs
@@ -1,24 +1,24 @@
 ﻿using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Dapr.Client;
 
-var baseURL = (Environment.GetEnvironmentVariable("BASE_URL") ?? "http://localhost") + ":" + (Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3500");
+using var client = new DaprClientBuilder().Build();
 
-var client = new HttpClient();
-client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-// Adding app id as part of the header
-client.DefaultRequestHeaders.Add("dapr-app-id", "order-processor");
+for (int i = 1; i <= 20; i++)
+{
+    var data = new { orderId = i };
 
-for (int i = 1; i <= 20; i++) {
-    var order = new Order(i);
-    var orderJson = JsonSerializer.Serialize<Order>(order);
-    var content = new StringContent(orderJson, Encoding.UTF8, "application/json");
-
+    var cts = new CancellationTokenSource();
+    Console.CancelKeyPress += (object? sender, ConsoleCancelEventArgs e) => cts.Cancel();
     // Invoking a service
-    var response = await client.PostAsync($"{baseURL}/orders", content);
-    Console.WriteLine("Order passed: " + order);
+    await client.InvokeMethodAsync<object>(
+        "order-processor",
+        "orders",
+        data,
+        cts.Token
+    );
+    Console.WriteLine("Order passed: " + i);
 
     await Task.Delay(TimeSpan.FromSeconds(1));
 }
-
-public record Order([property: JsonPropertyName("orderId")] int OrderId);
