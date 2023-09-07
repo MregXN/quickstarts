93ce506e8a265132d97d3b0b28167f94a71db03c
MregXN <mregxn@gmail.com>
2023-09-07 14:49:33 +0800

    javascript
    Signed-off-by: MregXN <mregxn@gmail.com>

--- service_invocation/javascript/http/checkout/index.js
+++ service_invocation/javascript/http/checkout/index.js
@@ -1,22 +1,23 @@
-import axios from "axios";
+import { DaprClient, HttpMethod } from "@dapr/dapr";
 
 const DAPR_HOST = process.env.DAPR_HOST || "http://localhost";
-const DAPR_HTTP_PORT = process.env.DAPR_HTTP_PORT || "3500";
 
 async function main() {
-  // Adding app id as part of the header
-  let axiosConfig = {
-    headers: {
-        "dapr-app-id": "order-processor"
-    }
-  };
-  
-  for(var i = 1; i <= 20; i++) {
-    const order = {orderId: i};
 
-    // Invoking a service
-    const res = await axios.post(`${DAPR_HOST}:${DAPR_HTTP_PORT}/orders`, order , axiosConfig);
-    console.log("Order passed: " + res.config.data);
+  const client = new DaprClient({ DAPR_HOST });
+
+  for (var i = 1; i <= 20; i++) {
+    const order = { orderId: i };
+
+
+    const r = await client.invoker.invoke(
+      "order-processor",
+      "orders",
+      HttpMethod.POST,
+      order,
+    );
+
+    console.log("Order passed: " + i);
 
     await sleep(1000);
   }
